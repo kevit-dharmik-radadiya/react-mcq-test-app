7fc0682aee7c97ab86f7cd0c94f271cf9bd5786a
Manish Andodariya <manish.andodariya@kevit.io>
2023-04-26 16:42:21 +0530

    feat: getQuestions form test Id adds

--- server/src/controllers/questionController.js
+++ server/src/controllers/questionController.js
@@ -12,3 +12,25 @@ exports.addQuestions = async (req, res, next) => {
     res.status(500).json({ message: 'Something went wrong!', success: false });
   }
 };
+
+exports.getQuestionsByTest = async (req, res, next) => {
+  try {
+    const { testId } = req.params;
+    const result = await Question.find({ testId: testId });
+    const response = result.map((e, index) => ({
+      _id: e._id,
+      number: index + 1,
+      question: e.questionText,
+      options: e.answers.map((ans) => ({ _id: ans._id, text: ans.answerText })),
+    }));
+    logger.log.info('Fetched questions by test' + testId, response);
+    res.status(201).json({
+      message: 'Questions fetched successfully!',
+      data: response || [],
+      success: true,
+    });
+  } catch (err) {
+    logger.log.error('500 - Questions fetch', err);
+    res.status(500).json({ message: 'Something went wrong!', success: false });
+  }
+};
--- server/src/routes/question.route.js
+++ server/src/routes/question.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth.middleware');
-const { addQuestions } = require('../controllers/questionController');
+const {
+  addQuestions,
+  getQuestionsByTest,
+} = require('../controllers/questionController');
 
 const router = express.Router();
 
 router.post('/addQuestions', authMiddleware, addQuestions);
+router.get('/getQuestionsByTest/:testId', authMiddleware, getQuestionsByTest);
 
 module.exports = router;
