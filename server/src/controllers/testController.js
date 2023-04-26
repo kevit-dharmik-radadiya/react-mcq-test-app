4492d803e00084cebfd481db54856977eee6d34d
Manish Andodariya <manish.andodariya@kevit.io>
2023-04-26 16:21:49 +0530

    feat: adds feature get tests by language

--- server/src/controllers/testController.js
+++ server/src/controllers/testController.js
@@ -5,13 +5,31 @@ exports.createTest = async (req, res, next) => {
   try {
     const { testName, language } = req.body;
 
-      const newTest = new Test({
-        testName,
-        language,
-      });
-      const result = await newTest.save();
-      logger.log.info('Test Created', result);
-      res.status(201).json({ message: 'Test created', success: true });
+    const newTest = new Test({
+      testName,
+      language,
+    });
+    const result = await newTest.save();
+    logger.log.info('Test Created', result);
+    res.status(201).json({ message: 'Test created', success: true });
+  } catch (err) {
+    logger.log.error('500 - test creation', err);
+    res.status(500).json({ message: 'Something went wrong!', success: false });
+  }
+};
+
+exports.getTests = async (req, res, next) => {
+  try {
+    const language = req.query.language;
+    const tests = await Test.find(
+      language ? { language: req.query.language } : {},
+    );
+    logger.log.info('Test Details', tests);
+    res.status(200).json({
+      message: 'Tests fetched successfully!',
+      data: tests || [],
+      success: true,
+    });
   } catch (err) {
     logger.log.error('500 - test creation', err);
     res.status(500).json({ message: 'Something went wrong!', success: false });
--- server/src/routes/test.routes.js
+++ server/src/routes/test.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth.middleware');
 const { getUserDetails } = require('../controllers/userController');
-const { createTest } = require('../controllers/testController');
+const { createTest, getTests } = require('../controllers/testController');
 
 const router = express.Router();
 
 router.post('/createTest', authMiddleware, createTest);
+router.get('/getTests', authMiddleware, getTests);
 
 module.exports = router;
