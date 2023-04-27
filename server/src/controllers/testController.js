13cbaf08af166ae9b00636a2a678e02a72434106
Manish Andodariya <manish.andodariya@kevit.io>
2023-04-27 11:49:22 +0530

    feat: adds submit test feature

--- server/src/controllers/testController.js
+++ server/src/controllers/testController.js
@@ -1,5 +1,8 @@
+const { Types } = require('mongoose');
+const Question = require('../models/questions.model');
 const Test = require('../models/test.modal');
 const logger = require('../services/logger');
+const UserScore = require('../models/userScore.model');
 
 exports.createTest = async (req, res, next) => {
   try {
@@ -28,6 +31,53 @@ exports.getTests = async (req, res, next) => {
     res.status(200).json({
       message: 'Tests fetched successfully!',
       data: tests || [],
+      success: true,
+    });
+  } catch (err) {
+    logger.log.error('500 - test creation', err);
+    res.status(500).json({ message: 'Something went wrong!', success: false });
+  }
+};
+
+exports.submitTest = async (req, res, next) => {
+  try {
+    const { testId, testData } = req.body;
+    const result = {
+      userId: req.userData.userId,
+      testId: testId,
+    };
+    const questionsByTest = await Question.aggregate([
+      { $match: { testId: Types.ObjectId(testId) } },
+      { $unwind: '$answers' },
+      { $match: { 'answers.isCorrect': true } },
+      { $project: { _id: 0, questionId: '$_id', answerId: '$answers._id' } },
+    ]);
+    const evaluation = questionsByTest?.reduce(
+      (acc, curr) => {
+        const isMatch = testData?.findIndex(
+          (e) =>
+            e?.questionId.toString() === curr?.questionId.toString() &&
+            e?.answerId.toString() === curr?.answerId.toString(),
+        );
+        return isMatch >= 0
+          ? {
+              testScore: acc.testScore + 1,
+              score: acc.score + 1,
+            }
+          : {
+              testScore: acc.testScore + 1,
+              score: acc.score,
+            };
+      },
+      { testScore: 0, score: 0 },
+    );
+    const scoreData = { ...evaluation };
+    scoreData.percentage = (evaluation.score / evaluation.testScore) * 100;
+    result.scoreDetails = scoreData;
+    const scoreToSave = new UserScore(result);
+    await scoreToSave.save();
+    res.status(200).json({
+      message: 'Tests submitted successfully!',
       success: true,
     });
   } catch (err) {
--- server/src/models/userScore.model.js
+++ server/src/models/userScore.model.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const userScoreSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   testId: { type: mongoose.Schema.Types.ObjectId, ref: 'Test' },
-  score: Number,
+  scoreDetails: {testScore: Number, score: Number, percentage: Number},
   dateTaken: { type: Date, default: Date.now },
-  lastLoggedInAt: Date
 });
 
 const UserScore = mongoose.model('UserScore', userScoreSchema);
