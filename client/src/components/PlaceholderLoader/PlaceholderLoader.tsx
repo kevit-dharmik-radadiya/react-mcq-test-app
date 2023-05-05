2b4954180f149fc9da0f851235f95e5d83660111
Dharmik <dharmik.radadiya@kevit.io>
2023-05-05 19:25:20 +0530

    Contact Us

--- client/src/components/PlaceholderLoader/PlaceholderLoader.tsx
+++ client/src/components/PlaceholderLoader/PlaceholderLoader.tsx
@@ -1,34 +1,59 @@
 import { Box } from "@mui/material";
 import ContentLoader from "react-content-loader";
 
-const PlaceholderLoader = () => {
+interface LoaderProps {
+  iteration: number;
+  boxWidth: string;
+  boxHeight: string;
+  canWidth: number;
+  canHeight: number;
+  boxShadow: boolean;
+  children: React.ReactNode;
+}
+
+const PlaceholderLoader = (props: LoaderProps) => {
+  const {
+    boxWidth,
+    iteration,
+    boxHeight,
+    canWidth,
+    canHeight,
+    boxShadow,
+    children,
+  } = props;
+  const iterationArray: number[] = Array.from(
+    { length: iteration },
+    (_, index: number) => index
+  );
+
   return (
-    <Box
-      sx={{
-        maxWidth: "270px",
-        width: "100%",
-        boxShadow: "0 0 10px #cbcbcb",
-        borderRadius: "10px",
-      }}
-    >
-      <ContentLoader
-        speed={2}
-        width={270}
-        height={133}
-        viewBox="0 0 270 133"
-        backgroundColor="#00a78321"
-        foregroundColor="#00a78380"
-      >
-        <rect x="20" y="27" rx="5" ry="5" width="130" height="20" />
-        <rect x="20" y="55" rx="5" ry="5" width="50" height="22" />
-        <circle cx="215" cy="49" r="35" />
-        <rect x="20" y="85" rx="3" ry="3" width="17" height="20" />
-        <rect x="46" y="85" rx="3" ry="3" width="17" height="20" />
-        <rect x="70" y="85" rx="3" ry="3" width="34" height="20" />
-        <rect x="195" y="92" rx="3" ry="3" width="17" height="20" />
-        <rect x="220" y="92" rx="3" ry="3" width="17" height="20" />
-      </ContentLoader>
-    </Box>
+    <>
+      {iterationArray.map((item) => {
+        return (
+          <Box
+            key={item}
+            sx={{
+              maxWidth: boxWidth,
+              width: "100%",
+              height: boxHeight,
+              boxShadow: boxShadow ? "0 0 10px #cbcbcb" : "none",
+              borderRadius: "10px",
+            }}
+          >
+            <ContentLoader
+              speed={2}
+              width={canWidth}
+              height={canHeight}
+              viewBox={`0 0 ${canWidth} ${canHeight}`}
+              backgroundColor="#00a78321"
+              foregroundColor="#00a78380"
+            >
+              {children}
+            </ContentLoader>
+          </Box>
+        );
+      })}
+    </>
   );
 };
 
